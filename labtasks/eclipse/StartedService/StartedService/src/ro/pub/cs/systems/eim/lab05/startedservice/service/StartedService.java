7031efa836ccb2aaab37e7efcf5f1fc447a53a38
Student User <student@cti.pub.ro>
2016-03-22 12:07:49 +0200

    implemented tasks of Laboratory 05

--- labtasks/eclipse/StartedService/StartedService/src/ro/pub/cs/systems/eim/lab05/startedservice/service/StartedService.java
+++ labtasks/eclipse/StartedService/StartedService/src/ro/pub/cs/systems/eim/lab05/startedservice/service/StartedService.java
@@ -20,7 +20,7 @@ public class StartedService extends Service {
         Log.d(Constants.TAG, "onStartCommand() method was invoked");
         // TODO: exercise 5 - implement and start the ProcessingThread
         Thread thread = new ProcessingThread(this);
-        thread.run();
+        thread.start();
         
         return START_REDELIVER_INTENT;
     }
